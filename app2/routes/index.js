ced22635708d807792235f72d300e2caf8f6879d
zcabeto <zcabeto@ucl.ac.uk>
2024-07-16 17:18:39 +0100

    input u&p

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -7,7 +7,20 @@ var InputSanitizer = require('./inputsanitizer');
 /* start page to view data */
 router.get('/', async function(req, res) {
   // frontend render on index.jade
-  res.render('index', { title: 'Database Website' });
+  res.render('index', { title: 'Databases Coursework', ACCEPT: false });
+});
+
+router.get('/login/:username/:password', async function(req, res) {
+  // frontend render on index.jade
+  let ACCEPT = false;
+  let username = req.params.username;
+  let password = req.params.password;
+  if (username && password) {
+    username = InputSanitizer.sanitizeString(username);
+    password = InputSanitizer.sanitizeString(password);
+    ACCEPT = true;
+  }
+  res.render('index', { title: 'Databases Coursework', ACCEPT: ACCEPT });
 });
 
 module.exports = router;
