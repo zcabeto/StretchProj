b60f389d57b29002296447f5c44fe087f7807e25
zcabeto <zcabeto@ucl.ac.uk>
2024-07-27 11:41:44 +0100

    login page simple

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -3,24 +3,20 @@
 var express = require('express');
 var router = express.Router();
 var InputSanitizer = require('./inputsanitizer');
+var LoginProcessor = require('./login');
 
-/* start page to view data */
 router.get('/', async function(req, res) {
-  // frontend render on index.jade
-  res.render('index', { title: 'Databases Coursework', ACCEPT: false });
+  res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT });
 });
 
-router.get('/login/:username/:password', async function(req, res) {
-  // frontend render on index.jade
-  let ACCEPT = false;
-  let username = req.params.username;
-  let password = req.params.password;
-  if (username && password) {
-    username = InputSanitizer.sanitizeString(username);
-    password = InputSanitizer.sanitizeString(password);
-    ACCEPT = true;
-  }
-  res.render('index', { title: 'Databases Coursework', ACCEPT: ACCEPT });
+router.get('/:username/:password', async function(req, res) {
+  LoginProcessor.login(req.params.username, req.params.password);
+  res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT });
+});
+
+router.get('/out', async function(req, res) {
+  LoginProcessor.logout();
+  res.render('index', { title: 'Stretch Project', ACCEPT: false });
 });
 
 module.exports = router;
