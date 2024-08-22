b52e924ab6fc703d0d3932207181fddd3f4b836f
zcabeto <zcabeto@ucl.ac.uk>
2024-08-22 20:49:03 +0100

    safer login send

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -6,28 +6,26 @@ var InputSanitizer = require('./inputsanitizer');
 var LoginProcessor = require('./login');
 
 router.get('/', async function(req, res) {
-  res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT, p: req.query.p || '000', h: req.query.h || '000', user: LoginProcessor.user || req.cookies['User'] });
-});
-
-router.get('/:username/:password', async function(req, res) {
-  let isHashed = parseInt((req.query.p || '000').charAt(0));
-  let SQLI = parseInt((req.query.p || '000').charAt(1));
-  let encode = parseInt((req.query.p || '000').charAt(2));
-  if (req.params.username && req.params.password) {
+  if (req.cookies['User'] != '' && req.cookies['User'] != null) {
+    let isHashed = parseInt((req.query.p || '000').charAt(0));
+    let SQLI = parseInt((req.query.p || '000').charAt(1));
+    let encode = parseInt((req.query.p || '000').charAt(2));
+    let acceptedCookie = req.cookies['User'].split(':');
     let username;
     let password;
     if (SQLI) {
-      username = InputSanitizer.sanitizeString(req.params.username, encode);
-      password = InputSanitizer.sanitizeString(req.params.password, encode);
+      username = InputSanitizer.sanitizeString(acceptedCookie[0], encode);
+      password = InputSanitizer.sanitizeString(acceptedCookie[1], encode);
     } else {
-      username = req.params.username;
-      password = req.params.password;
+      username = acceptedCookie[0];
+      password = acceptedCookie[1];
     }
-    console.log("User: " + username + ". Entered: "+req.params.username)
     let err = await LoginProcessor.login(username, password, isHashed);
     if (err != null) { res.render('error', { message: 'from login/', error: err}); }
+    res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT, p: req.query.p || '000', h: req.query.h || '000', user: LoginProcessor.user || username });
+  } else {
+    res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT, p: req.query.p || '000', h: req.query.h || '000', user: LoginProcessor.user });
   }
-  res.render('index', { title: 'Stretch Project', ACCEPT: LoginProcessor.ACCEPT, p: req.query.p || '000', h: req.query.h || '000', user: LoginProcessor.user || req.cookies['User'] });
 });
 
 router.get('/out', async function(req, res) {
