86b3cc868f87dc133c3821f341ea3489cce2743d
zcabeto <zcabeto@ucl.ac.uk>
2024-08-26 17:40:52 +0100

    Update CORS.js

--- app2/routes/CORS.js
+++ app2/routes/CORS.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var UrlSafeHolder = require('./urlSafety');
 
 router.get('/', async function(req, res) {
-  UrlSafeHolder.setCORS(parseInt(req.cookies['Protections'].charAt(2))>=1); 
+  UrlSafeHolder.setCORS(parseInt(req.cookies['Protections'].charAt(2))>=2); 
   UrlSafeHolder.setHTTPS(parseInt(req.cookies['Protections'].charAt(0))>=2);
   res.render('CORS', { title: 'CORS Link Page' });
 });
