0c31016e995aeffd9f9905ffe967238763eeda28
zcabeto <zcabeto@ucl.ac.uk>
2024-08-31 17:01:21 +0100

    finished guides

--- app2/routes/CORS.js
+++ app2/routes/CORS.js
@@ -7,7 +7,7 @@ var UrlSafeHolder = require('./urlSafety');
 router.get('/', async function(req, res) {
   UrlSafeHolder.setCORS(parseInt(req.cookies['Protections'].charAt(2))>=2); 
   UrlSafeHolder.setHTTPS(parseInt(req.cookies['Protections'].charAt(0))>=2);
-  res.render('CORS', { title: 'CORS Link Page' });
+  res.render('CORS', { title: 'External Page w/CORS Button', p: req.query.p || '000', h: req.query.h || '000' });
 });
 
 module.exports = router;
--- app2/routes/index.js
+++ app2/routes/index.js
@@ -9,7 +9,7 @@ router.get('/', async function(req, res) {
   let EncryptLv = parseInt((req.query.p || '000').charAt(0));
   let UrlSafeLv = parseInt((req.query.p || '000').charAt(2));
   UrlSafeHolder.setCORS(UrlSafeLv>=1); UrlSafeHolder.setHTTPS(EncryptLv>=2);
-  res.render('index', { title: 'Stretch Project', p: req.query.p || '000' });
+  res.render('index', { title: 'Stretch Project', p: req.query.p || '000', h: req.query.h || '000' });
 });
 
 module.exports = router;
