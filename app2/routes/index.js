f6bee4ec89b48f1a50d91c5475b64c48231d748f
zcabeto <zcabeto@ucl.ac.uk>
2024-09-19 11:09:10 +0100

    Tables remade

--- app2/routes/index.js
+++ app2/routes/index.js
@@ -19,16 +19,27 @@ router.get('/rebuild', async function(req, res) {
   UrlSafeHolder.setCORS(UrlSafeLv>=1); UrlSafeHolder.setHTTPS(EncryptLv>=2);
   let connection;
   try {
-    /*
     connection = await pool.getConnection();
-    const timeoutPromise = new Promise(resolve => { setTimeout(resolve, 5000); });
-    const rebuildDBPromise =  connection.execute();
-    await Promise.race([rebuildDBPromise, timeoutPromise]);
+    
+    await connection.execute(`CREATE TABLE IF NOT EXISTS Movies (movieId INT PRIMARY KEY, title VARCHAR(255));`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS Genres (genreId INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(100) UNIQUE);`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS MovieGenres (movieId INT,genreId INT,PRIMARY KEY (movieId, genreId),FOREIGN KEY (movieId) REFERENCES Movies(movieId),FOREIGN KEY (genreId) REFERENCES Genres(genreId));`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS Crew (movieId INT PRIMARY KEY,Director VARCHAR(100),TopTwoActors TEXT,releaseDate DATE,FOREIGN KEY (movieId) REFERENCES Movies(movieId));`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS Users (user VARCHAR(100) PRIMARY KEY,pass VARCHAR(100));`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS UsersHashed (user VARCHAR(100) PRIMARY KEY,pass VARCHAR(100));`);
+    await connection.execute(`CREATE TABLE IF NOT EXISTS Comments (commentId INT AUTO_INCREMENT PRIMARY KEY,writer VARCHAR(100),comment VARCHAR(255));`);
+    /*
+    await connection.execute(`LOAD DATA INFILE 'https://github.com/zcabeto/StretchProj/blob/main/data/cleaned_movies.csv' INTO TABLE Movies FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES (movieId, title);`);
+    await connection.execute(`LOAD DATA INFILE '/var/lib/mysql-files/genres.csv' INTO TABLE Genres FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (genreId, name);`);
+    await connection.execute(`LOAD DATA INFILE '/var/lib/mysql-files/movie_genres.csv' INTO TABLE MovieGenres FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES (movieId, genreId);`);
+    await connection.execute(`LOAD DATA INFILE '/var/lib/mysql-files/cleaned_crew.csv' INTO TABLE Crew FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;`);
+    await connection.execute(`LOAD DATA INFILE '/var/lib/mysql-files/users.csv' INTO TABLE Users FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;`);
+    await connection.execute(`LOAD DATA INFILE '/var/lib/mysql-files/usersHashed.csv' INTO TABLE UsersHashed FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;`);
     */
     res.render('index', { title: 'Stretch Project', p: req.query.p || '000', h: req.query.h || '000' });
   } catch (err) {
-    console.error('Error from data/', err);
-    res.render('error', { message: 'from data/', error: err});
+    console.error('Error from index/', err);
+    res.render('error', { message: 'from index/', error: err});
   } finally {
     if (connection) connection.release();
   }
